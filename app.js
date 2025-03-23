a002d4d5c1bc71bf30b746de91b7b60f4b12bf31
vignesh17-k <vignesh@sourcewiz.co>
2025-03-23 18:52:14 +0530

    fix: cors error

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const port = process?.env?.PORT || 5000;
 const corsOptions = {
   origin: '*', // Allow only this origin
   methods: ['GET', 'POST'],            // Allow only GET and POST methods
-  allowedHeaders: ['Content-Type'],    // Allow specific headers
+  allowedHeaders: ["Content-Type", "Authorization"], // ✅ Allow Authorization header
+  credentials: true,    // Allow specific headers
 };
 
 app.use(cors(corsOptions));
 
 app.use(express.json())
-app.use(cors());
 app.use('/api/products', require('./routes/getProducts'));
 app.use("/api/cart", require('./routes/cart'));
 app.use("/api/user", require('./routes/user'));
