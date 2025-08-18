a8320edb3c4c73086eef7d87c0798f11fff34510
vignesh17-k <vignesh@sourcewiz.co>
2025-08-18 20:49:47 +0530

    fix: refactor code

--- controller/productController.js
+++ controller/productController.js
@@ -16,17 +16,37 @@ const getProducts = (req, res) => {
 };
 
 const getPopularRails = (req, res) => {
+  const updated_popular_rails = mock_data.popular_rails.map((id) => {
+    const product = mock_data.products_data.find(
+      (product) => product?.id === id
+    );
+    return product;
+  });
+
   res.status(200).json({
     status: 200,
-    data: mock_data.popular_rails,
+    data: updated_popular_rails,
     message: "successfully fetched popular rails",
   });
 };
 
 const getMenuRails = (req, res) => {
+  const updated_menu_rails = mock_data.menu_data?.map((menu_item) => {
+    const updated_list = menu_item?.list?.map((id) => {
+      const product = mock_data.products_data.find(
+        (product) => product?.id === id
+      );
+      return product;
+    });
+    return {
+      ...menu_item,
+      list: updated_list,
+    };
+  });
+
   res.status(200).json({
     status: 200,
-    data: mock_data.menu_data,
+    data: updated_menu_rails,
     message: "successfully fetched menu rails",
   });
 };
